97c848d3c0e0a3090f156f8330be0042a7bd0c06
Surya Pratap Singh Chauhan <surya.pratap0038@gmail.com>
2023-08-31 07:47:47 +0530

    hash_id.cpp built

---
+++ temp/main temp/hash_id.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <unordered_map>
+#include <regex>
+#include <vector>
+
+using namespace std;
+
+unordered_map<string, string> hashTypes = {
+  {"MD5", "^[a-fA-F0-9]{32}$"},
+  {"SHA-1", "^[a-fA-F0-9]{40}$"},
+  {"SHA-256", "^[a-fA-F0-9]{64}$"},
+  {"SHA-512", "^[a-fA-F0-9]{128}$"},
+  {"CRC32", "^[a-fA-F0-9]{8}$"},
+  {"LM", "^[0-9A-F]{32}:[0-9A-F]{32}$"},
+  {"NTLM", "^[0-9A-F]{32}$"},
+  {"MySQL323", "^[0-9A-F]{16}$"},
+  {"PostgreSQL", "^[0-9A-F]{32}$"},
+  {"Cisco PIX", "^[0-9A-F]{16}$"},
+  {"RipeMD-160", "^[a-fA-F0-9]{40}$"},
+  {"Haval-128", "^[a-fA-F0-9]{32}$"},
+  {"MD4", "^[a-fA-F0-9]{32}$"},
+  {"GOST R 34.11-94", "^[a-fA-F0-9]{64}$"},
+  {"Whirlpool", "^[a-fA-F0-9]{128}$"},
+  {"CRC16-CCITT", "^[a-fA-F0-9]{4}$"},
+};
+
+vector<string> identifyHash(const string& hash) {
+    vector<string> possibleTypes;
+    for (const auto& entry : hashTypes) {
+        regex pattern(entry.second);
+        if (regex_match(hash, pattern))
+            possibleTypes.push_back(entry.first);
+    }
+    return possibleTypes;
+}
+
+int main() {
+    string inputHash;
+    cout << "Enter a hash: ";
+    cin >> inputHash;
+
+    vector<string> possibleTypes = identifyHash(inputHash);
+
+    if (possibleTypes.empty()) {
+        cout << "Unidentified hash\n";
+    } else if (possibleTypes.size() == 1) {
+        cout << "Hash identified as: " << possibleTypes[0] << '\n';
+    } else {
+        cout << "Hash can be from any of the following types:\n";
+        for (const auto& type : possibleTypes) {
+            cout << "- " << type << '\n';
+        }
+    }
+
+    return 0;
+}
