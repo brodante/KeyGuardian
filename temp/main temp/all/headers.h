5940d78d6ea8d62f3ab8f8172495f1b248ea269c
Surya Pratap Singh Chauhan <surya.pratap0038@gmail.com>
2023-08-31 19:12:59 +0530

    headers.h added

---
+++ temp/main temp/all/headers.h
@@ -0,0 +1,22 @@
+#ifndef CRYPTOPP_ALL_HEADERS_H
+#define CRYPTOPP_ALL_HEADERS_H
+
+#include<decry.cpp>
+#include<homescreen.cpp>
+#include<hash_id.cpp>
+
+#include <iostream>
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/md5.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/sha3.h>
+#include <cryptopp/ripemd.h>
+#include <cryptopp/whrlpool.h>
+#include <cryptopp/tiger.h>
+#include <cryptopp/panama.h>
+#include <cryptopp/ripemd.h>
+#include <cryptopp/crc.h>
+#include <cryptopp/gost.h>
+#include <cryptopp/keccak.h>
+
+#endif // CRYPTOPP_ALL_HEADERS_H
